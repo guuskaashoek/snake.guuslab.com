522785ca079ae23fe2e4ddcd3c55ce9348367a6e
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-08-07 20:50:22 +0200

    ds

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-window.addEventListener('deviceorientation', handleOrientation);
+// Functie om de oriëntatie van het apparaat bij te werken en de balk te draaien
+function updateRotation(event) {
+    var x = event.beta; // Kanteling rond de X-as (voorwaartse / achterwaartse beweging)
+    var y = event.gamma; // Kanteling rond de Y-as (zijwaartse beweging)
 
-function handleOrientation(event) {
-    var horizonTilt = event.beta || 0;
-    var waterpas = document.querySelector('.waterpas');
-    waterpas.style.transform = `rotate(${horizonTilt}deg)`;
+    var rotation = -y; // Inverseer de waarde voor draairichting
+
+    var rotatingBar = document.querySelector('.rotating-bar');
+    rotatingBar.style.transform = 'rotate(' + rotation + 'deg)';
 }
+
+// Luister naar het "deviceorientation" -evenement en roep de updateRotation-functie aan
+window.addEventListener('deviceorientation', updateRotation);
