8620c71f3230385d7685d04f4844e6011944bdc3
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-08-07 23:17:14 +0200

    SNAKE

--- script.js
+++ script.js
@@ -1,13 +1,168 @@
-// Functie om de oriëntatie van het apparaat bij te werken en de balk te draaien
-function updateRotation(event) {
-    var x = event.beta; // Kanteling rond de X-as (voorwaartse / achterwaartse beweging)
-    var y = event.gamma; // Kanteling rond de Y-as (zijwaartse beweging)
+const snakeElement = document.getElementById('snake');
+const foodElement = document.getElementById('food');
+const scoreElement = document.getElementById('score');
 
-    var rotation = -y; // Inverseer de waarde voor draairichting
+let snakeX = 2;
+let snakeY = 2;
+let foodX = 10;
+let foodY = 10;
+let snakeLength = 1;
+let snakeTrail = [{ x: snakeX, y: snakeY }];
+let directionX = 0;
+let directionY = 0;
+let gridSize = 15; // Aangepaste gridgrootte voor betere zichtbaarheid
 
-    var rotatingBar = document.querySelector('.rotating-bar');
-    rotatingBar.style.transform = 'rotate(' + rotation + 'deg)';
+let score = 0;
+let paused = false; // Definieer de variabele 'paused'
+
+document.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowUp':
+      directionX = 0;
+      directionY = -1;
+      break;
+    case 'ArrowDown':
+      directionX = 0;
+      directionY = 1;
+      break;
+    case 'ArrowLeft':
+      directionX = -1;
+      directionY = 0;
+      break;
+    case 'ArrowRight':
+      directionX = 1;
+      directionY = 0;
+      break;
+  }
+});
+
+function updateGameArea() {
+    snakeX += directionX;
+    snakeY += directionY;
+  
+    if (snakeX < 0) snakeX = gridSize - 1;
+    if (snakeX >= gridSize) snakeX = 0;
+    if (snakeY < 0) snakeY = gridSize - 1;
+    if (snakeY >= gridSize) snakeY = 0;
+  
+    snakeTrail.push({ x: snakeX, y: snakeY });
+  
+    // Keep the trail length equal to the snake length
+    while (snakeTrail.length > snakeLength) {
+      snakeTrail.shift();
+    }
+  
+    // Check for self-collision
+    for (let i = 0; i < snakeTrail.length - 1; i++) {
+      if (snakeTrail[i].x === snakeX && snakeTrail[i].y === snakeY) {
+        // Snake collided with itself, reset the game
+        snakeX = 2;
+        snakeY = 2;
+        snakeTrail = [{ x: snakeX, y: snakeY }];
+        directionX = 0;
+        directionY = 0;
+        snakeLength = 1;
+        score = 0;
+        scoreElement.textContent = score;
+      }
+    }
+  
+    if (snakeX === foodX && snakeY === foodY) {
+      score++;
+      snakeLength++;
+      generateFood();
+    }
+  
+    snakeElement.style.left = snakeX * 20 + 'px';
+    snakeElement.style.top = snakeY * 20 + 'px';
+  
+    const trailElements = document.querySelectorAll('.trail');
+    for (let i = 0; i < trailElements.length; i++) {
+      trailElements[i].parentNode.removeChild(trailElements[i]);
+    }
+  
+    for (let i = 0; i < snakeTrail.length; i++) {
+      const trailSegment = snakeTrail[i];
+      const trailSegmentElement = document.createElement('div');
+      trailSegmentElement.className = 'trail';
+      trailSegmentElement.style.left = trailSegment.x * 20 + 'px';
+      trailSegmentElement.style.top = trailSegment.y * 20 + 'px';
+      snakeElement.parentNode.insertBefore(trailSegmentElement, snakeElement);
+    }
+  
+    scoreElement.textContent = score;
+  }
+  
+
+
+  function generateFood() {
+    foodX = Math.floor(Math.random() * gridSize);
+    foodY = Math.floor(Math.random() * gridSize);
+    foodElement.style.left = foodX * 20 + 'px';
+    foodElement.style.top = foodY * 20 + 'px';
+  }
+  
+  generateFood();
+  setInterval(updateGameArea, 100);
+
+// ... je bestaande code ...
+
+// Voeg event listeners toe voor toetsen en knoppen
+document.getElementById('button-left').addEventListener('click', () => {
+    console.log('Button Left clicked');
+    changeDirection(-1, 0);
+});
+document.getElementById('button-right').addEventListener('click', () => changeDirection(1, 0));
+document.getElementById('button-up').addEventListener('click', () => changeDirection(0, -1));
+document.getElementById('button-down').addEventListener('click', () => changeDirection(0, 1));
+document.getElementById('button-pause').addEventListener('click', togglePause);
+
+// Functie om de richting van de slang te veranderen
+function changeDirection(newDirectionX, newDirectionY) {
+  if (paused) return;
+  if (newDirectionX === -directionX || newDirectionY === -directionY) return;
+
+  directionX = newDirectionX;
+  directionY = newDirectionY;
 }
 
-// Luister naar het "deviceorientation" -evenement en roep de updateRotation-functie aan
-window.addEventListener('deviceorientation', updateRotation);
+// Functie om het spel te pauzeren of hervatten
+function togglePause() {
+  paused = !paused;
+  if (paused) {
+    // Toon een pauze-scherm
+    const pauseScreen = document.createElement('div');
+    pauseScreen.id = 'pause-screen';
+    pauseScreen.textContent = 'PAUZE';
+    document.body.appendChild(pauseScreen);
+  } else {
+    // Verwijder het pauze-scherm
+    const pauseScreen = document.getElementById('pause-screen');
+    if (pauseScreen) {
+      pauseScreen.parentNode.removeChild(pauseScreen);
+    }
+  }
+}
+
+// Functie om toetsaanslagen te verwerken
+function handleKeyPress(event) {
+  const key = event.key.toLowerCase();
+  switch (key) {
+    case 'arrowleft':
+      changeDirection(-1, 0);
+      break;
+    case 'arrowright':
+      changeDirection(1, 0);
+      break;
+    case 'arrowup':
+      changeDirection(0, -1);
+      break;
+    case 'arrowdown':
+      changeDirection(0, 1);
+      break;
+    case ' ':
+      togglePause();
+      break;
+  }
+}
+window.addEventListener('keydown', handleKeyPress);
